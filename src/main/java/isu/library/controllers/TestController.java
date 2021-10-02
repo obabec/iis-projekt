67b5018efd0ea935b31dea34de86836757af39d6
Ondrej Babec <ond.babec@gmail.com>
2021-10-02 20:38:49 +0200

    Fix database source

--- src/main/java/isu/library/controllers/TestController.java
+++ src/main/java/isu/library/controllers/TestController.java
@@ -1,4 +1,13 @@
 package isu.library.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
 }
